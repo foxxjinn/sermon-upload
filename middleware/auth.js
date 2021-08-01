3b32a45f3ed668da4413d9af3e97e2ae7d8ee80a
foxxjinn <foxx.jinn@gmail.com>
2021-07-31 21:36:16 -0600

    playing around with auth and json web token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const route = express.Router()
 
+
 // Middleware that authorizes if the client is logged in.
 // it will see if client has Json Web Token.  If not, it will return
 // the login screen.
 
 route.get('/', (req, res, next) => {
-    let token = req.header('x-auth')
-    token = 'his'
+    let token = req.header('X-Auth')
 
-    if (!token || token !== 'hi') {
-        res.send('login')
+    if (!token || token !== '') {
+        res.sendFile('./public/html/login')
     }
 
     next()
 })
 
 module.exports = route
+
+// set a header
+// res.header('X-Auth', ${token})
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json({extended: false}))
 app.use(require('./middleware/auth'))
 
